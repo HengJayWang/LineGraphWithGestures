26fe347559e7488ee54b1eb3fa11060d5c71baf0
HengJay <hengjay.wang@itri.org.tw>
2022-03-13 21:22:36 +0800

    Initial Commit

---
+++ LineGraphWithGestures/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LineGraphWithGestures
+//
+//  Created by M200_Macbook_Pro on 2022/3/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LineGraphWithGestures/LineGraphWithGesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LineGraphWithGesturesApp.swift
+//  LineGraphWithGestures
+//
+//  Created by M200_Macbook_Pro on 2022/3/13.
+//
+
+import SwiftUI
+
+@main
+struct LineGraphWithGesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
